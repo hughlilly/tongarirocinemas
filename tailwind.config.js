7a92c1c975b62469081de1c2990cf27526e82972
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-09-08 18:41:19 +1200

    thurs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        wordmark: ["Aboreto", "cursive"],
+      },
+    },
   },
   plugins: [],
-}
+};
