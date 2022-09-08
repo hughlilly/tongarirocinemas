778d7deceb146d193e3917193a92ce2ab2cc7636
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-09-09 10:58:43 +1200

    js change

---
+++ src/scripts/mobile-menu.js
@@ -0,0 +1,21 @@
+/* 
+// Mobile menu
+// 
+// If js is not enabled, mobile users will
+// not be able to use the menu
+// 
+// Based on https://github.com/bradtraversy/tailwind-landing-page/
+// 
+*/
+
+// Get buttons to manipulate
+const hamButton = document.getElementById("hamburger-button");
+const mobileMenu = document.getElementById("mobile-menu");
+
+hamButton.addEventListener("click", () => {
+  // Turn hamburger menu button into 'close' cross after tap, and vice-versa
+  hamButton.classList.toggle("open");
+  // Toggle visibility of menu after tap (and vice-versa)
+  mobileMenu.classList.toggle("flex");
+  mobileMenu.classList.toggle("hidden");
+});
