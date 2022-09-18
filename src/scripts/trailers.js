40ac4c91e00c4d419e69921b0e772191145e29d0
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-09-18 20:35:28 +1200

    trailers js updates + comments

--- src/scripts/trailers.js
+++ src/scripts/trailers.js
@@ -1,7 +1,7 @@
 // Dynamically loads a YouTube iframe in a <dialog> element
 // at the end of the HTML file
 
-// Get iframe element to manipulate
+// Get iframe element so we can pause it on close
 const embeddedTrailer = document.getElementById("embedded-trailer");
 
 // Get "Play trailer" buttons as NodeList
@@ -25,6 +25,9 @@ trailerButtons.forEach((dialog) => {
     const modal = document.querySelector("#modal");
     modal.showModal();
 
+    // Set the body to "overflow: hidden" so that the document background behind the modal can't be scrolled
+    document.body.classList.toggle("modal-open");
+
     // When close button is clicked, also pause the video so that
     // it doesn't keep playing in the background; see
     // http://michaelrispolidevelopment.com/tips%20and%20tricks/2017/08/07/pause-youtube-video-on-an-event.html
@@ -36,6 +39,9 @@ trailerButtons.forEach((dialog) => {
       // Remove the "src" attribute so that when the modal is next displayed,
       // the previous trailer isn't shown (briefly)
       embeddedTrailer.removeAttribute("src");
+
+      // Remove the previously set "overflow: hidden" on the document body
+      document.body.classList.toggle("modal-open");
 
       // Close the modal
       modal.close();
